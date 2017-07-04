1e0f1ba71fee52417f1ab3db3d8b81493195f06c
wolf <vivanovic@gmail.com>
2017-07-05 01:42:59 +0200

    Add files via upload

---
+++ timer.js
@@ -0,0 +1,17 @@
+
+
+
+function podesi()
+{
+document.getElementById('demo').innerHTML = Date();
+}
+function convert(degree) {
+    var x;
+    if (degree == "C") {
+        x = document.getElementById("c").value * 9 / 5 + 32;
+        document.getElementById("f").value = Math.round(x);
+    } else {
+        x = (document.getElementById("f").value -32) * 5 / 9;
+        document.getElementById("c").value = Math.round(x);
+    }
+}
